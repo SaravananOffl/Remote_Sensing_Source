3e12211c9101da4c193a8273b5649e548d816a6b
Saravanan G <SGoffl1603@gmail.com>
2018-06-16 23:30:34 +0530

    image segmentor script refractored

---
+++ gdal_script.py
@@ -0,0 +1,61 @@
+import gdal
+import matplotlib.pyplot as plt
+import numpy as np
+
+path = r'H:\Bachmanity_Games\purdue\Input_Files\718\r1_0000_0004.tif'
+
+raster = gdal.Open(path)
+
+band = []
+for i in range(raster.RasterCount):
+        band.append(raster.GetRasterBand(i+1).ReadAsArray())
+
+band1 = band[0]  
+band2 = band[1]
+band3 = band[2] 
+band4 = band[3]
+
+for i in range(band1.shape[0]):
+    for j in range(band1.shape[1]):
+           if(band4[i][j]<=0.36): 
+                    band1[i][j] = 0
+                    band2[i][j] = 0
+                    band3[i][j] = 0
+                    band4[i][j] = 0
+
+plt.imshow(band4, 'copper')
+plt.title("Original")
+new_mat = np.count_nonzero(band4, axis = 0)
+new_mat = np.append(0,new_mat)
+new_mat = np.append(new_mat,0)
+print(new_mat)
+
+length_matrix = []
+if(np.count_nonzero(new_mat)>1):
+        print("Inside")
+        for i in range(new_mat.shape[0]-1):
+                if(new_mat[i] and new_mat[i-1]==0 and new_mat[i+1]!=0): # 011
+                        print(f'corres {i} {new_mat[i]}')
+                        for z in range(i+1,new_mat.shape[0]-1):
+                                print(f' after {z} ',new_mat[z])
+                                if(new_mat[z] and new_mat[z+1]==0):#10
+                                        value = (z-i+1)/2
+                                        if(value!=1):
+                                                if(value%2 ==0):
+                                                        value = int(value/2)
+                                                else:
+                                                        value =int((value+1)/2)
+                                                print(z-i+1, new_mat[i+value])
+                                                length_matrix.append(new_mat[i+value])
+                                                if((value*2)>9):
+                                                        print("INside")
+                                                        length_matrix.append(new_mat[i+value*2+value])
+                                                i = i +z 
+                                        break
+                
+
+else:
+        length_matrix.append("NaN")
+print(length_matrix)
+        
+plt.show()
